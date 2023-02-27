3ba2bf8d256982aa9a37ae35f983ca331e380496
MSI\ADMIN <huynhduy8122002@gmail.com>
2023-02-27 13:10:52 +0700

    push noon 27/02

---
+++ SpringMVCBai1Maven/src/main/java/hh/kk/WelcomeController.java
@@ -0,0 +1,12 @@
+package hh.kk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return "viewWelcome";
+	}
+}
